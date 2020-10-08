fbaa30f7a1dd838d9d410e423ff272750957d7bc
himawanTIF <40656518+himawanTIF@users.noreply.github.com>
2020-10-08 11:10:41 +0700

    Update 3
    - Finishing Hit Counter
    - Add GuestBook

--- counter.php
+++ counter.php
@@ -1,8 +1,9 @@
 <?php
 $filecounter="counter.txt";
-$fl=fopen($filecounter,"r+");
-$hit=fread($fl, filesize($filecounter));
+$fl=fopen($filecounter,"r+"); //Load file, r=read
+$hit=fread($fl, filesize($filecounter));//Fungsi hit membaca jumlah pengunjung dari file(fread)
 
+// Tampilan untuk melihat hit counter
 echo("<table width=250 align=center border=1 cellspacing=0 cellpadding=0 bordercolor=#0000FF><tr>");
 echo("<td width=250 valign=middle align=center>");
 echo("<font face=verdana size=2 color=#FF0000><b>");
@@ -13,8 +14,9 @@ echo("</td>");
 echo("</tr></table>");
 fclose($fl);
 
+// Menuliskan penambahan hit ke dalam file (fopen, hit+, fwrite) dan menutup file (fclose)
 $fl=fopen($filecounter,"w+");
 $hit=$hit+1;
-fwrite($fl,$hit,strlen($hit));
-fclose($fl);
+fwrite($fl,$hit,strlen($hit)); //Tulis ke dalam file
+fclose($fl); //Tutup file (fclose)
 ?>
