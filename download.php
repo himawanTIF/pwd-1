9f56907c2cc670696b0283d101c88d54b3b8d701
himawanTIF <40656518+himawanTIF@users.noreply.github.com>
2020-10-08 18:32:33 +0700

    Update 5
    - Penambahan Fungsi Upload
    - Penambahan Fungsi Download

---
+++ download.php
@@ -0,0 +1,12 @@
+<?php
+
+$myDir = "C:/xampp/htdocs/pwd-1/";
+$dir   = opendir($myDir);
+echo "Isi folder (klik link untuk download) : <br>";
+while($tmp = readdir($dir)){
+    echo "<a href='$tmp' target='_blank'>$tmp</a><br>";
+}
+
+closedir($dir);
+
+?>
